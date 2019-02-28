118ec4baec567dbb2b4830ff7dae947a3d814311
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-02-28 15:30:15 -0700

    channelManagement implements nextCommitment for non-app commitments

--- src/wallet/services/__test__/channelManagement.test.ts
+++ src/wallet/services/__test__/channelManagement.test.ts
@@ -1,143 +1,137 @@
-import { Commitment, sign, Signature, toHex } from 'fmg-core';
-import { bytesFromAppAttributes } from 'fmg-nitro-adjudicator';
+import { Bytes, sign, Signature, toHex } from 'fmg-core';
 import {
-    constructors as testDataConstructors,
-    funded_channel,
+  asCoreCommitment,
+  bytesFromAppAttributes,
+} from 'fmg-nitro-adjudicator';
+import {
+  constructors as testDataConstructors,
+  funded_channel,
 } from '../../../../test/test_data';
-import {
-    HUB_ADDRESS,
-    HUB_PRIVATE_KEY,
-    OTHER_PRIVATE_KEY,
-    PARTICIPANT_PRIVATE_KEY,
-} from '../../../constants';
+import { HUB_ADDRESS, HUB_PRIVATE_KEY } from '../../../constants';
+import { LedgerCommitment } from '../../../types';
 import { seeds } from '../../db/seeds/2_allocator_channels_seed';
 import AllocatorChannel from '../../models/allocatorChannel';
 import * as ChannelManagement from '../channelManagement';
 
 process.env.NODE_ENV = 'test';
 
-let pre_fund_setup_0: Commitment;
-let pre_fund_setup_1: Commitment;
-let post_fund_setup_0: Commitment;
-let theirSignature: Signature;
+let pre_fund_setup_0: LedgerCommitment;
+let pre_fund_setup_1: LedgerCommitment;
+let post_fund_setup_0: LedgerCommitment;
+let post_fund_setup_1: LedgerCommitment;
+let app_0: LedgerCommitment;
 let hubSignature: Signature;
 
 beforeEach(() => {
-    pre_fund_setup_0 = testDataConstructors.pre_fund_setup(0);
-    pre_fund_setup_1 = testDataConstructors.pre_fund_setup(1);
+  pre_fund_setup_0 = testDataConstructors.pre_fund_setup(0);
+  pre_fund_setup_1 = testDataConstructors.pre_fund_setup(1);
 
-    post_fund_setup_0 = testDataConstructors.post_fund_setup(2);
+  post_fund_setup_0 = testDataConstructors.post_fund_setup(2);
+  post_fund_setup_1 = testDataConstructors.post_fund_setup(3);
+  app_0 = testDataConstructors.app(4, funded_channel);
+
+  hubSignature = signAppCommitment(pre_fund_setup_1, HUB_PRIVATE_KEY);
 });
 
-describe('validateCommitment', () => {
-    it('returns true when the commitment was signed by the mover', async () => {
-        const signature = sign(toHex(pre_fund_setup_1), HUB_PRIVATE_KEY);
-        expect(
-            ChannelManagement.validSignature(pre_fund_setup_1, signature),
-        ).toBe(true);
-    });
-
-    it('returns false when the commitment was not signed by the mover', async () => {
-        const signature = sign(toHex(pre_fund_setup_0), HUB_PRIVATE_KEY);
-        expect(
-            ChannelManagement.validSignature(pre_fund_setup_0, signature),
-        ).toBe(false);
-    });
-
-    it('returns false when the commitment was not signed by the mover', async () => {
-        const signature = sign(toHex(pre_fund_setup_0), '0xf00');
-        expect(
-            ChannelManagement.validSignature(pre_fund_setup_0, signature),
-        ).toBe(false);
-    });
-});
+function signAppCommitment(c: LedgerCommitment, k: Bytes): Signature {
+  return sign(toHex(asCoreCommitment(c)), k);
+}
 
 describe('validSignature', () => {
-    it('returns true when the mover signed the commitment', async () => {
-        theirSignature = sign(toHex(pre_fund_setup_0), PARTICIPANT_PRIVATE_KEY);
-        hubSignature = sign(toHex(pre_fund_setup_1), HUB_PRIVATE_KEY);
+  it('returns true when the commitment was signed by the mover', async () => {
+    expect(
+      ChannelManagement.validSignature(
+        asCoreCommitment(pre_fund_setup_1),
+        hubSignature,
+      ),
+    ).toBe(true);
+  });
 
-        expect(
-            await ChannelManagement.validSignature(
-                pre_fund_setup_0,
-                theirSignature,
-            ),
-        ).toBe(true);
-        expect(
-            await ChannelManagement.validSignature(
-                pre_fund_setup_1,
-                hubSignature,
-            ),
-        ).toBe(true);
-    });
+  it('returns false when the commitment was not signed by the mover', async () => {
+    expect(
+      ChannelManagement.validSignature(
+        asCoreCommitment(pre_fund_setup_0),
+        hubSignature,
+      ),
+    ).toBe(false);
+  });
 
-    it('returns false when another participant signed the commitment', async () => {
-        theirSignature = sign(toHex(pre_fund_setup_1), PARTICIPANT_PRIVATE_KEY);
-        expect(
-            await ChannelManagement.validSignature(
-                pre_fund_setup_1,
-                theirSignature,
-            ),
-        ).toBe(false);
-    });
-
-    it('returns false when an unrelated participant signed the commitment', async () => {
-        theirSignature = sign(toHex(pre_fund_setup_0), OTHER_PRIVATE_KEY);
-        expect(
-            await ChannelManagement.validSignature(
-                pre_fund_setup_0,
-                theirSignature,
-            ),
-        ).toBe(false);
-    });
+  it('returns false when the commitment was not signed by the mover', async () => {
+    const signature = signAppCommitment(pre_fund_setup_0, '0xf00');
+    expect(
+      ChannelManagement.validSignature(
+        asCoreCommitment(pre_fund_setup_0),
+        signature,
+      ),
+    ).toBe(false);
+  });
 });
 
 describe('channelExists', () => {
-    it('returns true when a channel exists with the given (nonce, rules_address)', async () => {
-        expect(await ChannelManagement.channelExists(post_fund_setup_0)).toBe(
-            true,
-        );
-    });
+  it('returns true when a channel exists with the given (nonce, rules_address)', async () => {
+    expect(await ChannelManagement.channelExists(post_fund_setup_0)).toBe(true);
+  });
 
-    it('returns false when a channel exists with the given rules_address, but a different nonce', async () => {
-        const commitment = post_fund_setup_0;
-        expect(await ChannelManagement.channelExists(commitment)).toBe(true);
-        commitment.channel.nonce = 314;
-        expect(await ChannelManagement.channelExists(commitment)).toBe(false);
-    });
+  it('returns false when a channel exists with the given rules_address, but a different nonce', async () => {
+    const commitment = post_fund_setup_0;
+    expect(await ChannelManagement.channelExists(commitment)).toBe(true);
+    commitment.channel.nonce = 314;
+    expect(await ChannelManagement.channelExists(commitment)).toBe(false);
+  });
 
-    it('returns false when a channel exists with a different rules_address', async () => {
-        const commitment = post_fund_setup_0;
-        commitment.channel.channelType = HUB_ADDRESS;
-        expect(await ChannelManagement.channelExists(commitment)).toBe(false);
-    });
+  it('returns false when a channel exists with a different rules_address', async () => {
+    const commitment = post_fund_setup_0;
+    commitment.channel.channelType = HUB_ADDRESS;
+    expect(await ChannelManagement.channelExists(commitment)).toBe(false);
+  });
 });
 
 describe.skip('channelFunded', () => {
-    it('works', () => {
-        expect.assertions(1);
-    });
+  it('works', () => {
+    expect.assertions(1);
+  });
 });
 
 describe('formResponse', () => {
-    it('returns a signed core commitment', async () => {
-        const { rules_address, nonce } = seeds.funded_channel;
-        const channel = await AllocatorChannel.query()
-            .where({ rules_address, nonce })
-            .eager('commitments')
-            .first();
-        pre_fund_setup_1.channel = funded_channel;
+  it('returns a signed core commitment', async () => {
+    const { rules_address, nonce } = seeds.funded_channel;
+    const channel = await AllocatorChannel.query()
+      .where({ rules_address, nonce })
+      .eager('commitments')
+      .first();
+    pre_fund_setup_1.channel = funded_channel;
 
-        const signature = sign(toHex(pre_fund_setup_1), HUB_PRIVATE_KEY);
+    hubSignature = signAppCommitment(pre_fund_setup_1, HUB_PRIVATE_KEY);
 
-        expect(
-            await ChannelManagement.formResponse(
-                channel.id,
-                bytesFromAppAttributes,
-            ),
-        ).toMatchObject({
-            commitment: pre_fund_setup_1,
-            signature,
-        });
+    expect(
+      await ChannelManagement.formResponse(channel.id, bytesFromAppAttributes),
+    ).toMatchObject({
+      commitment: asCoreCommitment(pre_fund_setup_1),
+      signature: hubSignature,
     });
+  });
+});
+
+describe('nextCommitment', () => {
+  it('works on preFundSetup commitments', () => {
+    expect(ChannelManagement.nextCommitment(pre_fund_setup_0)).toMatchObject(
+      pre_fund_setup_1,
+    );
+  });
+
+  it('works on postFundSetup commitments', () => {
+    expect(ChannelManagement.nextCommitment(post_fund_setup_0)).toMatchObject(
+      post_fund_setup_1,
+    );
+  });
+
+  it('throws on app commitments', () => {
+    expect(() => ChannelManagement.nextCommitment(app_0)).toThrowError(
+      'CommitmentType.App',
+    );
+  });
+
+  it.skip('works on conclude commitments', () => {
+    expect.assertions(1);
+  });
 });
--- src/wallet/services/channelManagement.ts
+++ src/wallet/services/channelManagement.ts
@@ -3,8 +3,17 @@ import { HUB_PRIVATE_KEY } from '../../constants';
 import AllocatorChannel from '../models/allocatorChannel';
 import ConsensusCommitment from '../models/allocatorChannelCommitment';
 
-import { Commitment, mover, recover, sign, Signature, toHex } from 'fmg-core';
-import { AppAttrSanitizer } from '../../types';
+import {
+  BaseCommitment,
+  Commitment,
+  CommitmentType,
+  mover,
+  recover,
+  sign,
+  Signature,
+  toHex,
+} from 'fmg-core';
+import { AppAttrSanitizer, AppCommitment } from '../../types';
 export function validSignature(
   commitment: Commitment,
   signature: Signature,
@@ -13,7 +22,7 @@ export function validSignature(
 }
 
 export async function channelExists(
-  theirCommitment: Commitment,
+  theirCommitment: BaseCommitment,
 ): Promise<boolean> {
   const { channelType: rules_address, nonce } = theirCommitment.channel;
   const channel = await AllocatorChannel.query()
@@ -40,4 +49,47 @@ export async function formResponse(
   const signature = sign(commitment.toHex(sanitize), HUB_PRIVATE_KEY);
 
   return { commitment: commitment.asCoreCommitment(sanitize), signature };
+}
+
+export function nextCommitment(theirCommitment: AppCommitment): AppCommitment {
+  const appAttrType = typeof theirCommitment.appAttributes;
+
+  if (theirCommitment.commitmentType === CommitmentType.App) {
+    throw new Error('commitmentType cannot be CommitmentType.App');
+  }
+
+  let ourCommitment: typeof theirCommitment;
+  switch (theirCommitment.commitmentType) {
+    case CommitmentType.PreFundSetup:
+      ourCommitment = {
+        ...theirCommitment,
+        turnNum: theirCommitment.turnNum + 1,
+        commitmentCount: theirCommitment.commitmentCount + 1,
+      };
+
+      break;
+    case CommitmentType.PostFundSetup:
+      ourCommitment = {
+        ...theirCommitment,
+        turnNum: theirCommitment.turnNum + 1,
+        commitmentCount: theirCommitment.commitmentCount + 1,
+      };
+      break;
+
+    case CommitmentType.Conclude:
+      ourCommitment = {
+        ...theirCommitment,
+        turnNum: theirCommitment.turnNum + 1,
+        commitmentCount: theirCommitment.commitmentCount + 1,
+      };
+  }
+
+  if (typeof ourCommitment.appAttributes !== appAttrType) {
+    // TODO: Does this actually enforce that the types match?
+    throw new Error(
+      'Type error: typeof ourCommitment.appAttributes must match typeof theirCommitment.appAttributes',
+    );
+  }
+
+  return ourCommitment;
 }
