b13ac02e2bcd1dba805d6e6ffb83138745b3fcdd
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-02-28 23:25:32 -0700

    Add formResponse test

--- src/wallet/__test__/wallet.test.ts
+++ src/wallet/__test__/wallet.test.ts
@@ -1,10 +1,14 @@
-import { Commitment } from 'fmg-core';
 import Wallet from '..';
-import { constructors as testDataConstructors } from '../../../test/test_data';
+import {
+  constructors as testDataConstructors,
+  funded_channel,
+} from '../../../test/test_data';
+import { LedgerCommitment } from '../../types';
+import AllocatorChannel from '../models/allocatorChannel';
 
 process.env.NODE_ENV = 'test';
 
-let pre_fund_setup_0: Commitment;
+let pre_fund_setup_0: LedgerCommitment;
 
 beforeEach(() => {
   pre_fund_setup_0 = testDataConstructors.pre_fund_setup(0);
@@ -14,5 +18,19 @@ describe('sanitize', () => {
   it('sanitizes application attributes with the sanitize method it was passed', async () => {
     const wallet = new Wallet(() => '0xf00');
     expect(wallet.sanitize(pre_fund_setup_0.appAttributes)).toEqual('0xf00');
+  });
+});
+
+describe('formResponse', () => {
+  it('sanitizes application attributes with the sanitize method it was passed', async () => {
+    const wallet = new Wallet(() => '0xf00d');
+    const channel = await AllocatorChannel.query()
+      .where({
+        nonce: funded_channel.nonce,
+      })
+      .select('id')
+      .first();
+    const response = await wallet.formResponse(channel.id);
+    expect(response.commitment.appAttributes).toEqual('0xf00d');
   });
 });
