14e56afd5126246726d70fb52215a952838317ac
Mike Kerzhner <mikerzhner@gmail.com>
2019-03-01 12:21:21 -0800

    Commenting out error lines

--- src/app/services/rpsChannelManager.ts
+++ src/app/services/rpsChannelManager.ts
@@ -7,39 +7,39 @@ import AllocatorChannelCommitment from "../../wallet/models/allocatorChannelComm
 import { AppAttributes, asCoreCommitment, fromCoreCommitment, Play, PositionType, RPSBaseCommitment, RPSCommitment } from "./rps-commitment";
 
 export async function openLedgerChannel(theirCommitment: Commitment, theirSignature: Signature): Promise<ChannelResponse> {
-    if (await ChannelManagement.channelExists(theirCommitment)) {
-      throw errors.CHANNEL_EXISTS;
-    }
+  if (await ChannelManagement.channelExists(theirCommitment)) {
+    throw errors.CHANNEL_EXISTS;
+  }
 
-    if (!ChannelManagement.validSignature(theirCommitment, theirSignature)) {
-      throw errors.COMMITMENT_NOT_SIGNED;
-    }
+  if (!ChannelManagement.validSignature(theirCommitment, theirSignature)) {
+    throw errors.COMMITMENT_NOT_SIGNED;
+  }
 
-    const allocator_channel = await queries.openAllocatorChannel(theirCommitment);
-    return ChannelManagement.formResponse(allocator_channel);
+  const allocator_channel = await queries.openAllocatorChannel(theirCommitment);
+  return ChannelManagement.formResponse(allocator_channel);
 }
 
 export async function updateLedgerChannel(theirCommitment: Commitment, theirSignature: Signature): Promise<ChannelResponse> {
-    if (!ChannelManagement.validSignature(theirCommitment, theirSignature)) {
-        throw errors.COMMITMENT_NOT_SIGNED;
-    }
+  if (!ChannelManagement.validSignature(theirCommitment, theirSignature)) {
+    throw errors.COMMITMENT_NOT_SIGNED;
+  }
 
-    if (!(await ChannelManagement.channelExists(theirCommitment))) {
-        throw errors.CHANNEL_MISSING;
-    }
+  if (!(await ChannelManagement.channelExists(theirCommitment))) {
+    throw errors.CHANNEL_MISSING;
+  }
 
-    if (!await valuePreserved(theirCommitment)) {
-        throw errors.VALUE_LOST;
-    }
+  if (!await valuePreserved(theirCommitment)) {
+    throw errors.VALUE_LOST;
+  }
 
-    if (!await validTransition(theirCommitment)) {
-      throw errors.INVALID_TRANSITION;
-    }
+  if (!await validTransition(theirCommitment)) {
+    throw errors.INVALID_TRANSITION;
+  }
 
-    const ourCommitment = nextCommitment(theirCommitment);
+  const ourCommitment = nextCommitment(theirCommitment);
 
-    const allocator_channel = await queries.updateAllocatorChannel(theirCommitment, ourCommitment);
-    return ChannelManagement.formResponse(allocator_channel);
+  const allocator_channel = await queries.updateAllocatorChannel(theirCommitment, ourCommitment);
+  return ChannelManagement.formResponse(allocator_channel);
 }
 
 export function nextCommitment(theirCommitment: Commitment): Commitment {
@@ -59,12 +59,12 @@ export function nextCommitment(theirCommitment: Commitment): Commitment {
       };
 
     case CommitmentType.App:
-    const ourMove = move(fromCoreCommitment(theirCommitment));
+      const ourMove = move(fromCoreCommitment(theirCommitment));
       return {
         ...theirCommitment,
         turnNum: theirCommitment.turnNum + 1,
         commitmentCount: 0,
-        appAttributes: asCoreCommitment(ourMove).appAttributes,
+        appAttributes: null //asCoreCommitment(ourMove).appAttributes,
       };
     case CommitmentType.Conclude:
       return {
@@ -79,14 +79,14 @@ function move(theirPosition: AppAttributes): AppAttributes {
   switch (theirPosition.positionType) {
     case PositionType.Resting:
       const salt = '0xabc';
-      const aPlay;
+      const aPlay = null;
       return {
         stake: theirPosition.stake,
         positionType: PositionType.Proposed,
         salt: '0x',
         preCommit: preCommit(Play.Paper, salt),
         aPlay,
-        bPlay,
+        bPlay: aPlay,
       };
     case PositionType.Proposed:
       return theirPosition;
@@ -102,21 +102,21 @@ function preCommit(play: Play, salt: Bytes32): Bytes32 {
 }
 
 export async function valuePreserved(theirCommitment: any): Promise<boolean> {
-    return theirCommitment && true;
+  return theirCommitment && true;
 }
 
 export async function validTransition(theirCommitment: Commitment): Promise<boolean> {
   const { channel } = theirCommitment;
   const allocator_channel_id = (await AllocatorChannel.query()
-  .where({rules_address: channel.channelType, nonce: channel.nonce})
-  .select("id")
-  .first()).id;
+    .where({ rules_address: channel.channelType, nonce: channel.nonce })
+    .select("id")
+    .first()).id;
 
   const currentCommitment = await AllocatorChannelCommitment.query()
-  .where({ allocator_channel_id })
-  .orderBy("id", "desc")
-  .select()
-  .first();
+    .where({ allocator_channel_id })
+    .orderBy("id", "desc")
+    .select()
+    .first();
 
   return theirCommitment.turnNum === currentCommitment.turn_number + 1;
 }
--- src/wallet/utilities/blockchain.ts
+++ src/wallet/utilities/blockchain.ts
@@ -1,6 +1,7 @@
-import * as CommitmentArtifact from '../../../build/contracts/Commitment.json';
+/*import * as CommitmentArtifact from '../../../build/contracts/Commitment.json';
 import * as nitroAdjudicatorArtifact from '../../../build/contracts/NitroAdjudicator.json';
 import * as RulesArtifact from '../../../build/contracts/Rules.json';
+*/
 
 import { ContractFactory, providers } from "ethers";
 import { getNetworkId, linkedByteCode, } from "magmo-devtools";
@@ -12,25 +13,27 @@ const provider = new providers.JsonRpcProvider(`http://${process.env.DEV_GANACHE
 const providerSigner = provider.getSigner();
 
 export async function nitroAdjudicator() {
-  return setupContract(nitroAdjudicatorArtifact);
+  //return setupContract(nitroAdjudicatorArtifact);
+  return null;
 }
 
 async function setupContract(artifact: any) {
   const networkId = await getNetworkId();
 
+  /*Object.defineProperty(
+    artifact,
+    "bytecode",
+    { value: linkedByteCode(artifact, CommitmentArtifact, networkId) }
+  );
   Object.defineProperty(
     artifact,
-    "bytecode", 
-   {value: linkedByteCode(artifact, CommitmentArtifact, networkId)}
-   );
-  Object.defineProperty(
-    artifact,
-    "bytecode", 
-   {value: linkedByteCode(artifact, RulesArtifact, networkId)}
-   );
+    "bytecode",
+    { value: linkedByteCode(artifact, RulesArtifact, networkId) }
+  );
 
   const nitroFactory = await ContractFactory.fromSolidity(artifact, providerSigner);
   const contract = await nitroFactory.attach(artifact.networks[networkId].address);
 
-  return contract;
+  return contract;*/
+  return null;
 }
