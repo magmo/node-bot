8c77edbb51fd43764b93037479b28bf783ec9dda
Andrew Stewart <andrew.gord.stewart@gmail.com>
2019-03-01 17:25:26 -0700

    knex setup/teardown is sync

--- src/config/knexSetupTeardown.ts
+++ src/config/knexSetupTeardown.ts
@@ -1,18 +1,18 @@
-import knex from "../wallet/db/connection";
+import knex from '../wallet/db/connection';
 
-beforeEach(async () => {
-  await knex.migrate.rollback();
-  await knex.migrate.latest();
-  await knex.seed.run();
+beforeAll(() => {
+  return knex.migrate.rollback().then(() => {
+    return knex.migrate.latest();
+  });
 });
 
-afterEach(async () => {
-  await knex.migrate.rollback();
+beforeEach(() => {
+  return knex.seed.run();
 });
 
-afterAll(async () => {
+afterAll(() => {
   // We need to close the db connection after the test suite has run.
   // Otherwise, jest will not exit within the required one second after the test
   // suite has finished
-  await knex.destroy();
+  return knex.destroy();
 });
