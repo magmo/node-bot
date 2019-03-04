6ce11eb08aff228fe7a74e814c06eaf85892d1b7
Mike Kerzhner <mikerzhner@gmail.com>
2019-03-04 08:37:30 -0800

    Adding staging environment

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
 const BASE_PATH = path.join(__dirname, sourceDirectory, 'wallet', 'db');
 const CONN_STRING = `${process.env.SERVER_WALLET_DB_CONN_STRING}/${
   process.env.SERVER_WALLET_DB_NAME
-}`;
+  }`;
 
 module.exports = {
   test: {
@@ -35,6 +35,19 @@ module.exports = {
     migrations: {
       directory: path.join(BASE_PATH, 'migrations'),
       extensions: ['ts'],
+    },
+    seeds: {
+      directory: path.join(BASE_PATH, 'seeds'),
+    },
+    debug: process.env.DEBUG_KNEX === 'TRUE',
+  },
+
+  staging: {
+    client: 'pg',
+    connection: CONN_STRING,
+    migrations: {
+      directory: path.join(BASE_PATH, 'migrations'),
+      extensions: ['js'],
     },
     seeds: {
       directory: path.join(BASE_PATH, 'seeds'),
